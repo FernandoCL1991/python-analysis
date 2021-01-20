07962e7de419d8346e698e812c471b7755a8c1fa
FernandoCL1991 <73721626+FernandoCL1991@users.noreply.github.com>
2021-01-19 23:25:43 -0600

    Added final file of PyBank.

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-#CREATING FILE PATH
-import os
+## PYBANK SCRIPT ## FINISHED
 
-#IMPORTING CSV READING MODULE
+##IMPORTING MODULES TO RUN THE SCRIPT
+import os
+import math
+from statistics import mean
+
+##IMPORTING CSV READING MODULE
 import csv
 
 #CREATING THE PATH TO THE DOCUMENT
@@ -10,18 +14,53 @@ print(csvpath)
 
 #CREATING VARIABLES
 sum_months=[]
-total=[]
+net_change_list=[]
+
 
 #READ THE CSV FILE
-with open(csvpath, "r") as csvfile:
-    csvreader=csv.reader(csvfile,delimiter=",")
+with open(csvpath, 'r') as csvfile:
+    csvreader=csv.reader(csvfile, delimiter=',')
 
-    #CONSIDER THE DATA AFTER THE HEADER , AND SAVING IT AS A HEADER
+    #CONSIDER THE DATA AFTER THE HEADER , AND SAVING IT AS HEADER
     csvheader= next(csvreader)
+    first_row = next(csvreader)
 
-    #RETRIEVING TOTAL DATES IN DATASET
+    current_net = first_row[1]
+    max_change = 0
+    min_change = 0
+
+    
     for row in csvreader:
-        sum_months.append(row[0])
-    print(f"The total number of months in the dataset is: {len(sum_months)}")
+        sum_months.append(row[0]) #TOTAL NUMBER OF MONTHS IN THE DATASET.
+        previous_net = current_net #SETTING EQUALITY TO START THE FIRST LOOP AND ALSO APPLYING IT TO THE UPCOMING ONES.
+        current_net=row[1]
+        net_change = float(current_net) - float(previous_net) #NET TOTAL AMOUNT OF PROFIT/LOSSES OVER THE PERIOD.
+        net_change_list.append(net_change)
+        if net_change > max_change: #GREATEST INCREASE IN PROFIT.
+            max_change = net_change
+        if net_change < min_change: #GREATEST DECREASE IN PROFIT.
+            min_change = net_change
 
 
+net_change_average = mean(net_change_list) #RETRIEVING net_change AND APPLYING mean TO GET AVERAGE OF NET CHANGES.
+total_amount= sum(net_change_list) #TOTAL AMOUNT OF NET CHANGES.
+
+output_file = (
+    "Financial Analysis"    
+    "----------------------------------"
+    f"Total Months: {len(sum_months)}"
+    f"Total: ${total_amount}"
+    f"Average Change: ${net_change_average}")
+print(output_file)
+
+#PRINT ANALYSIS TO THE TERMINAL AND EXPORT A TEXT FILE WITH THE RESULTS.
+#Open the output file
+with open(output_file, "w", newline="") as datafile:
+    writer = csv.writer(datafile)
+
+    # Write the header row
+    writer.writerow(["Date", "Profit/Losses"])
+
+    # Write in zipped rows
+    writer.writerows(cleaned_csv)
+    
