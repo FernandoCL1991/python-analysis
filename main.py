06b9567a04a6b4854e024d72a2c13dec14931dc7
FernandoCL1991 <73721626+FernandoCL1991@users.noreply.github.com>
2021-01-21 22:41:21 -0600

    Added PyPoll doc. Still work to do in dictionaries

--- main.py
+++ main.py
@@ -1,65 +1,34 @@
-## PYBANK SCRIPT ## FINISHED
+## PYPOLL SCRIPT ## WORK IT!!!!   COPIA ORIGINAL
 
+#%%
 ##IMPORTING MODULES TO RUN THE SCRIPT
 import os
-import math
-from statistics import mean
-
-##IMPORTING CSV READING MODULE
 import csv
 
+##CREATING VARIABLES
+total_votes=[]
+unique_list=[]
+candDict={}
+
+
 #CREATING THE PATH TO THE DOCUMENT
-csvpath= os.path.join("Resources", "budget_data.csv")
+csvpath = os.path.join ("Resources", "election_data.csv")
 print(csvpath)
 
-#CREATING VARIABLES
-sum_months=[]
-net_change_list=[]
-
 #READ THE CSV FILE
-with open(csvpath, 'r') as csvfile:
-    csvreader=csv.reader(csvfile, delimiter=',')
+with open(csvpath, "r") as datafile: 
+    csvreader = csv.reader(datafile, delimiter= ",")
 
-    #CONSIDER THE DATA AFTER THE HEADER , AND SAVE IT AS HEADER
-    csvheader= next(csvreader)
-    
-    first_row = next(csvreader)
+    #OMIT THE HEADER AND STORE IT AS A VARIABLE
+    csvheader = next(csvreader)
 
-    current_net = first_row[1]
-    
-    max_change = 0
-    min_change = 0
-
-    
+    counter= 0
+    #FOR EACH ROW IN csv.reader:
     for row in csvreader:
-        sum_months.append(row[0]) #TOTAL NUMBER OF MONTHS IN THE DATASET.
-        previous_net = current_net #SETTING EQUALITY TO START THE FIRST LOOP AND ALSO APPLYING IT TO THE UPCOMING ONES.
-        current_net=row[1]
-
-        net_change = float(current_net) - float(previous_net) #NET TOTAL AMOUNT OF PROFIT/LOSSES OVER THE PERIOD.
-        net_change_list.append(net_change)
-        if net_change > max_change: #GREATEST INCREASE IN PROFIT.
-            max_change = net_change
-        if net_change < min_change: #GREATEST DECREASE IN PROFIT.
-            min_change = net_change
-
-
-net_change_average = mean(net_change_list) #RETRIEVING net_change AND APPLYING mean TO GET AVERAGE OF NET CHANGES.
-total_amount= sum(net_change_list) #TOTAL AMOUNT OF NET CHANGES.
-
-output_file = (
-    "Financial Analysis"    
-    "----------------------------------"
-    f"Total Months: {len(sum_months)}"
-    f"Total: ${total_amount}"
-    f"Average Change: ${net_change_average}")
-print(output_file)
-
-#PRINT ANALYSIS TO THE TERMINAL AND EXPORT A TEXT FILE WITH THE RESULTS.
-
-with open(output_file, "w", newline="") as datafile: #OPEN OUTPUT FILE.
-    writer = csv.writer(datafile)
-
-    # WRITE THE HEADER ROW.
-    writer.writerow(["Date", "Profit/Losses"])
-    
+        counter=counter+1
+        candidates=row[2] ##TOTAL NUMBER OF VOTES.
+        if candidates not in unique_list:
+            unique_list.append(candidates) ##LIST OF CANDIDATES WHO RECEIVED VOTES.
+       
+print(f"The total number of votes is: {counter}.") ##TOTAL NUMBER OF VOTES.
+print(f"The entire list of candidates is: {str(unique_list)}.") ##COMPLETE LIST OF CANDIDATES.
